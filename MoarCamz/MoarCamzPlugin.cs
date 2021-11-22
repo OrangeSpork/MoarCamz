72a962831065105ea3d011fea694dc3947cb9990
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-11-22 11:32:46 -0700

    Reverted required HS2API version to 1.26 to match last repack correctly.

--- MoarCamz/MoarCamzPlugin.cs
+++ MoarCamz/MoarCamzPlugin.cs
@@ -24,7 +24,7 @@ namespace MoarCamz
     {
         public const string GUID = "orange.spork.moarcamzplugin";
         public const string PluginName = "MoarCamz";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
 
         public static MoarCamzPlugin Instance { get; private set; }
 
